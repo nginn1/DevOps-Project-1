63cf526d0364064f2db23c0f058cc482ef27276f
nginn1 <ngyt1997@gmail.com>
2025-03-04 18:35:15 -0500

    Get Tests to 100% Code Coverage
    1) Get Collection tests to 100% code coverage
    2) Get Issue tests to 100% code coverage
    3) Get Series tests to 100% code coverage

--- code/Nathan Ginn Project 1/src/test/java/edu/westga/comp4420/comic_book_collection_app/test/model/collection/TestCollectionConstructor.java
+++ code/Nathan Ginn Project 1/src/test/java/edu/westga/comp4420/comic_book_collection_app/test/model/collection/TestCollectionConstructor.java
@@ -3,6 +3,7 @@ package edu.westga.comp4420.comic_book_collection_app.test.model.collection;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Random;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,32 @@ class TestCollectionConstructor {
 		
 		assertTrue(collection1.addSeries(series1));
 		assertTrue(collection1.addIssue(issue1));
+		
+		List<Series> collection1SeriesList = collection1.getSeriesList();
+		List<Issue> collection1IssueList = collection1.getIssueList();
+		
+		assertEquals(collection1SeriesList.size(), 1);
+		assertEquals(collection1IssueList.size(), 1);
+	}
+	
+	@Test
+	public void testSettersWithNullInput() {
+		Collection collection1 = new Collection();
+		
+		assertFalse(collection1.addSeries(null));
+		assertFalse(collection1.addIssue(null));
+	}
+	
+	@Test
+	public void testSettersWithAlreadyContainedInput() {
+		Collection collection1 = new Collection();
+		Series series1 = new Series("Batman", "Scott Snyder", "Greg Capullo", "DC");
+		Issue issue1 = new Issue("Batman", 1, "Scott Snyder", "Greg Capullo", "DC");
+		
+		collection1.addSeries(series1);
+		collection1.addIssue(issue1);
+		
+		assertFalse(collection1.addSeries(series1));
+		assertFalse(collection1.addIssue(issue1));
 	}
 }
--- code/Nathan Ginn Project 1/src/test/java/edu/westga/comp4420/comic_book_collection_app/test/model/series/TestSeriesConstructor.java
+++ code/Nathan Ginn Project 1/src/test/java/edu/westga/comp4420/comic_book_collection_app/test/model/series/TestSeriesConstructor.java
@@ -60,4 +60,61 @@ class TestSeriesConstructor {
 			Series series1 = new Series("Batman", "Scott Snyder", "Greg Capullo", "");
 		});
 	}
+	
+	@Test
+	public void testSettersWithValidInput() {
+		Series series1 = new Series("Batman", "Scott Snyder", "Greg Capullo", "DC");
+		
+		series1.setTitle("X-Men");
+		series1.setWriter("Chris Claremont");
+		series1.setArtist("Jim Lee");
+		series1.setPublisher("Marvel");
+		
+		assertEquals(series1.getTitle(), "X-Men");
+		assertEquals(series1.getWriter(), "Chris Claremont");
+		assertEquals(series1.getArtist(), "Jim Lee");
+		assertEquals(series1.getPublisher(), "Marvel");
+	}
+	
+	@Test
+	public void testSettersWithNullInput() {
+		Series series1 = new Series("Batman", "Scott Snyder", "Greg Capullo", "DC");
+		
+		assertThrows(NullPointerException.class, () -> {
+			series1.setTitle(null);
+		});
+		
+		assertThrows(NullPointerException.class, () -> {
+			series1.setWriter(null);
+		});
+		
+		assertThrows(NullPointerException.class, () -> {
+			series1.setArtist(null);
+		});
+		
+		assertThrows(NullPointerException.class, () -> {
+			series1.setPublisher(null);
+		});
+	}
+	
+	@Test
+	public void testSettersWithEmptyInput() {
+		Series series1 = new Series("Batman", "Scott Snyder", "Greg Capullo", "DC");
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			series1.setTitle("");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			series1.setWriter("");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			series1.setArtist("");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			series1.setPublisher("");
+		});
+	}
 }
