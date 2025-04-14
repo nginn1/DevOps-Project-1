ad4f1a5979b41f95e433c168edf5d0707b1d9a97
nginn1 <ngyt1997@gmail.com>
2025-04-14 13:02:01 -0400

    Create Launcher.java
    1) add launcher class to launch the application without error from main class.

---
+++ code/Nathan Ginn Project 1/src/main/java/edu/westga/comp4420/comic_book_collection_app/Launcher.java
@@ -0,0 +1,16 @@
+package edu.westga.comp4420.comic_book_collection_app;
+
+/**
+ * Entry point for the executable
+ *
+ *
+ */
+public class Launcher {
+	
+	/**
+	 * Executable java entry point.
+	 */
+	public static void main(string[] args) {
+		Main.main(args);
+	}
+}
